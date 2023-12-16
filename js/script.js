baf2733c34759386ba7b7322a733adad0b29d4ec
annisadwiarum <annisadwiarum710@gmail.com>
2023-12-16 14:51:52 +0700

    make header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// Toggle class active untuk hamburger menu
+// toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
+
+//ketika humberger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+    navbarNav.classList.toggle('active');
 };
+
+//klik di luar sidebar bisa mengclose sidebar otomatis
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+        navbarNav.classList.remove('active');
+})
