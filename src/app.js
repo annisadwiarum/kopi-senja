f7232eb5b314331ed67ae01fe2f0964aedba9fad
annisadwiarum <annisadwiarum710@gmail.com>
2023-12-30 16:39:16 +0700

    update cart feature

--- src/app.js
+++ src/app.js
@@ -8,6 +8,36 @@ document.addEventListener('alpine:init', () => {
             { id: 5, name: 'Coffee Beans 5', img: 'product-01.jpg', price: 25000 },
         ],
     }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            //mengecek apakah ada barang di dalam cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // jika belum ada, alias kosong
+            if (!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+            } else {
+                // mengecek jika barang sudah ada, berangnya beda atau sama
+                this.item = this.items.map((item) => {
+                    // jika barang beda
+                    if (item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang ternyata sama, cukup tambah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+    });
 });
 
 // konversi ke rupiah
