5f8a2b94e9be0cab7faba5b76b77e61bd7b624e8
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-05 17:00:08 +0700

    configuration midtrans

--- src/app.js
+++ src/app.js
@@ -68,6 +68,61 @@ document.addEventListener('alpine:init', () => {
     });
 });
 
+// form validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function() {
+    for(let i = 0; i < form.elements.length; i++) {
+        if (form.elements[i].value.length !== 0) {
+            checkoutButton.classList.remove('disabled');
+            checkoutButton.classList.add('disabled');
+        } else {
+            return false;
+        }
+    }
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove('disabled');
+});
+
+// kirim data ketika tombol checkout di klik.s
+checkoutButton.addEventListener('click', async function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objectData = Object.fromEntries(data);
+    // const message = formatMessage(objectData);
+    // window.open('http://wa.me/6285835300072?text=' + encodeURIComponent(message));
+
+    // request transsaction token dengan ajax / fetch
+    try {
+        const response = await fetch('php/placeOrder.php', {
+            method: 'POST',
+            body: data,
+        });
+        const token = await response.text();
+        console.log(token);
+        // window.snap.pay('TRANSACTION_TOKEN_HERE');
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+});
+
+// format pesan whatsapp
+const formatMessage = (object) => {
+    return `Data Customer
+        Nama: ${object.name}
+        Email: ${object.email}
+        Phone: ${object.phone}
+Data Pesanan
+        ${JSON.parse(object.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
+TOTAL: ${rupiah(object.total)}
+Syukran katsiran :)`;
+};
+
 // konversi ke rupiah
 const rupiah = (number) => {
     return new Intl.NumberFormat('id-ID', {
