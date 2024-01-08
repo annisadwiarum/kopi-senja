1b2a7a3ab035ca5e0051cb0c519d8f8df4c82664
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-08 17:02:11 +0700

    update

--- php/placeOrder.php
+++ php/placeOrder.php
@@ -13,7 +13,7 @@ require_once dirname(__FILE__) . '/midtrans-php-master/Midtrans.php';
 //SAMPLE REQUEST START HERE
 
 // Set your Merchant Server Key
-\Midtrans\Config::$serverKey = 'Mid-server-ApC3VHVgiYMYoaq2BEllc8Vc';
+\Midtrans\Config::$serverKey = 'SB-Mid-server-ApC3VHVgiYMYoaq2BEllc8Vc';
 // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
 \Midtrans\Config::$isProduction = false;
 // Set sanitization on (default)
