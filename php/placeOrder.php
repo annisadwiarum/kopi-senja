b7944f646f5c4a55caee4db23986bd713b73d45d
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-09 14:18:20 +0700

    fix bug for get the token

--- php/placeOrder.php
+++ php/placeOrder.php
@@ -13,7 +13,7 @@ require_once dirname(__FILE__) . '/midtrans-php-master/Midtrans.php';
 //SAMPLE REQUEST START HERE
 
 // Set your Merchant Server Key
-\Midtrans\Config::$serverKey = 'SB-Mid-server-ApC3VHVgiYMYoaq2BEllc8Vc';
+\Midtrans\Config::$serverKey = 'SB-Mid-server-HJHBeCovDjs08a3gUr9OVaxE';
 // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
 \Midtrans\Config::$isProduction = false;
 // Set sanitization on (default)
